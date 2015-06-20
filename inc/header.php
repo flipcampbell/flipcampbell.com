7c89e278d01a61c890f301afc399068562990e9b
Philip Campbell <flipcampbell@gmail.com>
2015-06-19 23:06:59 -0500

    Changed urls to be root relative

--- inc/header.php
+++ inc/header.php
@@ -6,19 +6,19 @@
     <meta name="keywords" content="freelance, developer, front end, front-end developer, HTML, CSS, PHP, Javascript, jQuery, Wordpress, Nashville, Tennessee, mobile, responsive, Foundation, Bootstrap, HTML5, CSS3" />
     <meta name="description" content="Personal Website of Freelance Web Developer, Philip Campbell. I specialize in responsive HTML, CSS, Javascript, jQuery, PHP, and Wordpress. I am also a drummer in a local jazz band." />
     <title>Philip Campbell | Freelance Developer | Nashville, TN</title>
-    <link rel="stylesheet" href="css/normalize.css" />
-    <link rel="stylesheet" href="./css/foundation.css" />
+    <link rel="stylesheet" href="/css/normalize.css" />
+    <link rel="stylesheet" href="/css/foundation.css" />
     <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,700' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Fira+Sans:400,500,500italic' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="./css/custom.css">
-    <script src="./js/vendor/modernizr.js"></script>
+    <link rel="stylesheet" href="/css/custom.css">
+    <script src="/js/vendor/modernizr.js"></script>
   </head>
   <body>
     <header class="bg-charcoal">
       <div class="row">
         <div class="small-6 small-centered medium-5 medium-centered large-4 large-centered columns">
-          <a href="http://www.flipcampbell.com"><img src="img/flipcampbell.svg" alt="Flip Campbell"></a>
+          <a href="http://www.flipcampbell.com"><img src="/img/flipcampbell.svg" alt="Flip Campbell"></a>
         </div>
       </div>
     </header>
