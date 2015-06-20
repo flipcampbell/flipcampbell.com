00afc4228a4cf13ac523c8666638a1569b6066bd
Philip Campbell <flipcampbell@gmail.com>
2015-06-19 23:06:01 -0500

    Backed up old version of site

---
+++ Backup/inc/footer.php
@@ -0,0 +1,35 @@
+<footer class="charcoal">
+      <div class="row">
+        <div class="small-7 medium-4 large-3 large-centered small-centered medium-centered columns">
+          <div class="small-6 medium-6 large-6 columns social-buttons">
+            <a href="http://www.linkedin.com/pub/philip-campbell/9a/398/1a9/" target=_BLANK><img src="img/socials-linkedin.svg" alt="LinkedIn"></a>
+          </div>
+          <div class="small-6 medium-6 large-6 social-buttons columns">
+            <a href="https://www.twitter.com/flipcampbell" target=_BLANK><img src="img/socials-twitter.svg" alt="Twitter"></a>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="small-12 small-centered columns">
+          <p class="center white">Copyright &copy; <?php echo date ('Y'); ?> Philip Campbell. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-40997726-3', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script src="js/app.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
