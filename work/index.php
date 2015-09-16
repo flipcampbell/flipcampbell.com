dcb51df5ca7a4dbca3a320fb65e08e40ee799bdd
Philip Campbell <flipcampbell@gmail.com>
2015-09-15 21:52:51 -0500

    added thumbnails to work gallery

--- work/index.php
+++ work/index.php
@@ -6,42 +6,42 @@
       </p>
     </div>
     <div class="bg-white">
-      <div class="row">
+      <div class="row center">
         <div class="large-6 large-uncentered small-12 small-centered columns">
-          <a href="agriscaping/" class="center">
-            <img src="" alt="">
+          <a href="agriscaping/">
+            <img src="/img/thumbnail-agriscaping.png" alt="">
             <h4>MyAgriscapePro Mobile App</h4>
             <h5>HTML5, SCSS, AngularJS, Ionic Framework</h5>
           </a>
         </div>
         <div class="large-6 large-uncentered small-12 small-centered columns">
-          <a href="lambworks/" class="center">
-            <img src="" alt="">
+          <a href="lambworks/">
+            <img src="/img/thumbnail-lambworks.png" alt="">
             <h4>Lambworks.com</h4>
             <h5>HTML5, SCSS, jQuery, PHP</h5>
           </a>
         </div>
       </div>
-      <div class="row">
+      <div class="row center">
         <div class="large-6 large-uncentered small-12 small-centered columns">
-          <a href="hawkhvac/" class="center">
-            <img src="" alt="">
+          <a href="hawkhvac/">
+            <img src="/img/thumbnail-hawkhvac.png" alt="">
             <h4>Hawkhvac.com</h4>
             <h5>HTML5, SCSS, jQuery, PHP</h5>
           </a>
         </div>
         <div class="large-6 large-uncentered small-12 small-centered columns">
-          <a href="ymcamontgomery/" class="center">
-            <img src="" alt="">
+          <a href="ymcamontgomery/">
+            <img src="/img/thumbnail-ymca.png" alt="">
             <h4>Ymcamontgomery.org</h4>
             <h5>HTML5, CSS, WordPress(Genesis Framework)</h5>
           </a>
         </div>
       </div>
-      <div class="row">
+      <div class="row center">
         <div class="large-6 large-uncentered small-12 small-centered columns">
-          <a href="d2s/" class="center">
-            <img src="" alt="">
+          <a href="d2s/">
+            <img src="/img/thumbnail-d2s.png" alt="">
             <h4>2014 D2S Fearless Landing Page</h4>
             <h5>HTML5, CSS, WordPress(Genesis Framework)</h5>
           </a>
