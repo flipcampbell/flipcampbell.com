499641db56f8f2a075b82c6e0f3574f2a2e4881a
Philip Campbell <flipcampbell@gmail.com>
2015-09-15 20:50:02 -0500

    began work page template

---
+++ work/index.php
@@ -0,0 +1,68 @@
+<?php include('../inc/header.php'); ?>
+
+    <div class="hero container">
+      <p class="small-12 small-centered columns center text-white">
+        Selected Projects
+      </p>
+    </div>
+    <div class="bg-white">
+      <div class="row">
+        <div class="large-6 large-uncentered small-12 small-centered columns">
+          <a href="work/agriscaping/" class="center">
+            <img src="" alt="">
+            <h4>MyAgriscapePro Mobile App</h4>
+            <h5>HTML5, SCSS, AngularJS, Ionic Framework</h5>
+          </a>
+        </div>
+        <div class="large-6 large-uncentered small-12 small-centered columns">
+          <a href="work/lambworks/" class="center">
+            <img src="" alt="">
+            <h4>Lambworks.com</h4>
+            <h5>HTML5, SCSS, jQuery, PHP</h5>
+          </a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="large-6 large-uncentered small-12 small-centered columns">
+          <a href="work/agriscaping/" class="center">
+            <img src="" alt="">
+            <h4>MyAgriscapePro Mobile App</h4>
+            <h5>HTML5, SCSS, AngularJS, Ionic Framework</h5>
+          </a>
+        </div>
+        <div class="large-6 large-uncentered small-12 small-centered columns">
+          <a href="work/lambworks/" class="center">
+            <img src="" alt="">
+            <h4>Lambworks.com</h4>
+            <h5>HTML5, SCSS, jQuery, PHP</h5>
+          </a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="large-6 large-uncentered small-12 small-centered columns">
+          <a href="work/agriscaping/" class="center">
+            <img src="" alt="">
+            <h4>MyAgriscapePro Mobile App</h4>
+            <h5>HTML5, SCSS, AngularJS, Ionic Framework</h5>
+          </a>
+        </div>
+        <div class="large-6 large-uncentered small-12 small-centered columns">
+          <a href="work/lambworks/" class="center">
+            <img src="" alt="">
+            <h4>Lambworks.com</h4>
+            <h5>HTML5, SCSS, jQuery, PHP</h5>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="bg-light-grey">
+      <div class="row">
+        <h3 class="large-8 large-uncentered small-12 small-centered columns center">
+          Need help bringing your designs to life?
+        </h3>
+        <div class="large-4 large-uncentered small-12 small-centered columns center">
+          <a href="/#contact" class="small radius button bg-aqua submit-button">Let's Talk</a>
+        </div>
+      </div>
+    </div>
+<?php include('../inc/footer.php') ?>
