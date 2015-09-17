1bac3464651fcdb3957371d036cc0043263e362c
Philip Campbell <flipcampbell@gmail.com>
2015-09-16 21:01:34 -0500

    added styles for work gallery

--- work/index.php
+++ work/index.php
@@ -1,60 +1,60 @@
 <?php include('../inc/header.php'); ?>
 
-    <div class="hero container">
+    <div class="hero padding-top-5 padding-bottom-4">
       <p class="small-12 small-centered columns center text-white">
         Selected Projects
       </p>
     </div>
-    <div class="bg-white">
+    <div class="bg-white gallery">
       <div class="row center">
-        <div class="large-6 large-uncentered small-12 small-centered columns">
+        <div class="large-6 large-uncentered small-12 small-centered columns padding-top-5">
           <a href="agriscaping/">
             <img src="/img/thumbnail-agriscaping.png" alt="">
-            <h4>MyAgriscapePro Mobile App</h4>
-            <h5>HTML5, SCSS, AngularJS, Ionic Framework</h5>
+            <h4 class="text-blue-green uppercase padding-top-1">MyAgriscapePro Mobile App</h4>
+            <h5 class="text-charcoal">HTML5, SCSS, AngularJS, Ionic Framework</h5>
           </a>
         </div>
-        <div class="large-6 large-uncentered small-12 small-centered columns">
+        <div class="large-6 large-uncentered small-12 small-centered columns padding-top-5">
           <a href="lambworks/">
             <img src="/img/thumbnail-lambworks.png" alt="">
-            <h4>Lambworks.com</h4>
-            <h5>HTML5, SCSS, jQuery, PHP</h5>
+            <h4 class="text-blue-green uppercase padding-top-1">Lambworks.com</h4>
+            <h5 class="text-charcoal">HTML5, SCSS, jQuery, PHP</h5>
           </a>
         </div>
       </div>
       <div class="row center">
-        <div class="large-6 large-uncentered small-12 small-centered columns">
+        <div class="large-6 large-uncentered small-12 small-centered columns padding-top-5">
           <a href="hawkhvac/">
             <img src="/img/thumbnail-hawkhvac.png" alt="">
-            <h4>Hawkhvac.com</h4>
-            <h5>HTML5, SCSS, jQuery, PHP</h5>
+            <h4 class="text-blue-green uppercase padding-top-1">Hawkhvac.com</h4>
+            <h5 class="text-charcoal">HTML5, SCSS, jQuery, PHP</h5>
           </a>
         </div>
-        <div class="large-6 large-uncentered small-12 small-centered columns">
+        <div class="large-6 large-uncentered small-12 small-centered columns padding-top-5">
           <a href="ymcamontgomery/">
             <img src="/img/thumbnail-ymca.png" alt="">
-            <h4>Ymcamontgomery.org</h4>
-            <h5>HTML5, CSS, WordPress(Genesis Framework)</h5>
+            <h4 class="text-blue-green uppercase padding-top-1">Ymcamontgomery.org</h4>
+            <h5 class="text-charcoal">HTML5, CSS, WordPress (Genesis Framework)</h5>
           </a>
         </div>
       </div>
       <div class="row center">
-        <div class="large-6 large-uncentered small-12 small-centered columns">
+        <div class="large-6 large-uncentered small-12 small-centered columns padding-top-5 padding-bottom-5">
           <a href="d2s/">
             <img src="/img/thumbnail-d2s.png" alt="">
-            <h4>2014 D2S Fearless Landing Page</h4>
-            <h5>HTML5, CSS, WordPress(Genesis Framework)</h5>
+            <h4 class="text-blue-green uppercase padding-top-1">2014 D2S Fearless Landing Page</h4>
+            <h5 class="text-charcoal">HTML5, CSS, WordPress (Genesis Framework)</h5>
           </a>
         </div>
       </div>
     </div>
-    <div class="bg-light-grey">
+    <div class="bg-light-grey padding-top-5 padding-bottom-4 gallery">
       <div class="row">
-        <h3 class="large-8 large-uncentered small-12 small-centered columns center">
+        <h3 class="large-8 large-uncentered small-12 small-centered columns center text-dark-blue">
           Need help bringing your designs to life?
         </h3>
         <div class="large-4 large-uncentered small-12 small-centered columns center">
-          <a href="/#contact" class="small radius button bg-aqua submit-button">Let's Talk</a>
+          <a href="/#contact" class="radius button bg-aqua button-xl">Let's Talk</a>
         </div>
       </div>
     </div>
