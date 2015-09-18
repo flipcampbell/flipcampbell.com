aaafe0cc603514f2ddcf0d9e9b8d86eefc0dbcf7
Philip Campbell <flipcampbell@gmail.com>
2015-09-17 22:15:16 -0500

    added retina support

--- inc/footer.php
+++ inc/footer.php
@@ -30,6 +30,7 @@
     </script>
     <script src="/bower_components/jquery/dist/jquery.min.js"></script>
     <script src="/bower_components/foundation/js/foundation.min.js"></script>
+    <script type="text/javascript" src="/js/retina.js"></script>
     <script src="/js/app.js"></script>
     <script src="/js/init.js"></script>
     <script>
